421a5ca81d661ef365a76ea9e2bc23fe36dd4b43
tinyhu <huruiw@outlook.com>
2026-03-10 22:48:37 +0800

    chore(release): bump version to 0.2.29
    - fix UTF-8 BOM issue in release metadata files

--- src/constants/version.js
+++ src/constants/version.js
@@ -1,4 +1,4 @@
-﻿export const APP_VERSION = '0.2.28';
+export const APP_VERSION = '0.2.29';
 export const APP_VERSION_LABEL = `V${APP_VERSION}`;
 export const RELEASE_REPO = 'tinyhu666/Lingo';
 export const RELEASE_API_URL = `https://api.github.com/repos/${RELEASE_REPO}/releases/latest`;
