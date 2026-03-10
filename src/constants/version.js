b57fcc002c57c323646ba877197238a9c06ce2b3
hurui <hurui@tiny.local>
2026-03-10 17:55:34 +0800

    chore: prepare v0.2.26 release

--- src/constants/version.js
+++ src/constants/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '0.2.25';
+export const APP_VERSION = '0.2.26';
 export const APP_VERSION_LABEL = `V${APP_VERSION}`;
 export const RELEASE_REPO = 'tinyhu666/Lingo';
 export const RELEASE_API_URL = `https://api.github.com/repos/${RELEASE_REPO}/releases/latest`;
